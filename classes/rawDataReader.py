60d79630b64f5aa8db165257d8350fa4818d3285
oaalanism <88028598+oaalanism@users.noreply.github.com>
2022-02-14 01:57:02 +0100

    Update rawDataReader.py

--- classes/rawDataReader.py
+++ classes/rawDataReader.py
@@ -2,13 +2,39 @@ import os
 import numpy as np
 
 class RawDataReader:
-
+    """
+        This object reads raw data format
+        
+        Parameters
+        ----------
+    """
+    def __init__(self, path):
+        self.path = path
+        
     def getFiles(self):
+        """
+            Function to get the names of raw data files 
+            
+            Parameters
+            ----------
+            
+            Returns
+            -------
+        """
         self.files = [self.path + f for f in os.listdir(self.path)]
         self.files = sorted(self.files, key=lambda x: float(x.split("-")[1][:-4]))
 
 
     def getFilesNumpyData(self):
+        """
+            Extract depth data from files
+            
+            Parameters
+            ----------
+            
+            Returns
+            -------
+        """
         self.depth_frames = []
 
         for file in self.files:
@@ -18,9 +44,16 @@ class RawDataReader:
         
 
     def start(self):
+        """
+            function that starts data extraction 
+            
+            Parameters
+            ----------
+            
+            Extract
+            -------
+        """
         self.getFiles()
         self.getFilesNumpyData()
         return self.depth_frames
 
-    def __init__(self, path):
-        self.path = path
